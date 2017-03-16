30daefe0f3f8b086f4be29c65342cbc3698b40ad
Valentin Alexeev <valentin.m.alexeev@gmail.com>
2017-03-16 14:32:36 +0000

    First commit book list react redux app

---
+++ README.md
@@ -0,0 +1,10 @@
+# Book Lisk  App with React/Redux
+
+Install App
+
+```
+	> git clone https://github.com/val-alexeev/books-list-react-redux-app.git
+	> cd books-list-react-redux-app
+	> npm install
+	> npm start
+```
---
+++ src/reducers/reduce_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: 'JavaScript: The Good Parts', pages: 172},
+    { title: 'Eloquent JavaScript', pages: 490},
+    { title: `You Don't Know JS`, pages: 72},
+    { title: 'Programming JavaScript Applications', pages: 253}
+  ];
+}
